6aa005b894a424ff929c9e123efcc894eed33b6e
Felix Orinda <forinda82@gmail.com>
2025-01-25 15:02:07 +0300

    Update tests

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,6 +1,21 @@
 <template>
   <div>basic</div>
+  <swal-pop
+    :message="message"
+    class="p-6 bg-green-400 rounded-xl shadow-md w-full"
+    @send-message="sendMessage"
+  />
 </template>
 
 <script setup>
+const app = useNuxtApp()
+const message = 'Message with Composition API'
+const sendMessage = (message) => {
+  app.$swal_mx.fire({
+    title: 'Message received',
+    text: message,
+    icon: 'success',
+  })
+  console.log('Received message:', message)
+}
 </script>
